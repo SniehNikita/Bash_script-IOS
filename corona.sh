f256e3770db19482d9ff6301b7451c4c50c90f42
xsnieh00 <xsnieh00@stud.fit.vutbr.cz>
2022-03-26 20:30:52 +0100

    Aded all main params read

--- corona.sh
+++ corona.sh
@@ -1,94 +1,149 @@
 #!/bin/bash
 
+
+# -h function
 Helper() {
-	echo "Usage: $0 [-h] [FILTERS] [COMMAND] [LOG [LOG2 [...]]" 1>&2; exit; 
+	echo "Usage: $0 [-h] [FILTERS] [COMMAND] [LOG [LOG2 [...]]" 1>&2;
+    echo "-h : Help. (You see this message now)"
+    echo ""
+    echo "FILTERS can be:"
+    echo "-a date (YYYY-MM-DD) : search after this date"
+    echo "-b date (YYYY-MM-DD) : search before this date"
+    echo "-g GENDER (M (for men) or Z (for women)) : searches for only one gender"
+    echo "-s [WIDTH](>0) : for gender, age, daily, monthly, yearly, countries, districts and regions commands shows histogram" 
+
+    exit 0;
 }
 
+
+
+# Prints error massage, given as $1, to stderr
 CallError() {
     echo $1 >&2
 }
 
-IsTimeCorrect() {
-    if [ ! `date "+%Y-%m-%d" -d $1` ];
+
+
+# Checks date, given as $1, according to the format, given as $2
+IsDateCorrect() {
+    if [ ! `date -d "$1" +$2` ];
     then
-        echo "[TODO ERROR] Wrong time format";exit;        
+        CallError "[TODO ERROR] Wrong time format";
     fi
 }
 
-c=false
-a=false
-b=false
-s=false
 
 
-i=1;
-j=$#;
-while [ $i -le $j ] 
-do
-    #echo $1
-    case $1 in
-        -h )
-            Helper
-            ;;
-        infected | merge | gender | age | daily | monthly | yearly | countries | districts | regions )
-            if [[ $c = false ]]
-            then 
-                c=true
-                COMMAND=$1;
-            else 
-                echo "[TODO ERROR] Only one command PLEASE";exit 1;
-            fi
-            ;;
-        -a ) 
-            if [[ $a = false ]]
-            then 
-                i=$((i + 1));
-                a=true
-                shift 1;
-                TIMEAFTER=$1;
-                IsTimeCorrect $TIMEAFTER
-            else 
-                echo "[TODO ERROR] Only one -a paraPAPAm";exit 1;
-            fi
-            ;;
-        -b ) 
-            if [[ $b = false ]]
-            then 
-                i=$((i + 1));
-                b=true
-                shift 1;
-                TIMEBEFORE=$1;
-                IsTimeCorrect $TIMEBEFORE
-            else 
-                echo "[TODO ERROR] Only one -b paraPAPAm";exit 1;
-            fi
-            ;;
-        -s )
-            if [[ $s = false ]]
-            then
-                if [[ $2 =~ ^[0-9]+$ ]]
-                then
-                    s=true 
-                    WIDTH=$2
-                    shift;
-                    i=$((i + 1))
-                else
-                    s=true
-                    WIDTH=-1
+# Looks for params
+FindParams() {
+    local i=1;
+    local j=$#;
+    while [ $i -le $j ] 
+    do
+        case $1 in
+            -h )
+                Helper
+                ;;
+            infected | merge | gender | age | daily | monthly | yearly | countries | districts | regions )
+                if [[ $c = false ]]
+                then 
+                    c=true
+                    COMMAND=$1;
+                else 
+                    CallError "[TODO ERROR] Only one command PLEASE";
                 fi
-            else
-                echo "[TODO ERROR] Only one -s";exit 1;
-            fi
-            ;;
+                ;;
+            -a ) 
+                if [[ $a = false ]]
+                then 
+                    i=$((i + 1));
+                    a=true
+                    shift;
+                    TIMEAFTER=$1;
+                    IsDateCorrect $TIMEAFTER %Y-%m-%d
+                else 
+                    CallError "[TODO ERROR] Only one -a paraPAPAm";
+                fi
+                ;;
+            -b ) 
+                if [[ $b = false ]]
+                then 
+                    i=$((i + 1));
+                    b=true
+                    shift;
+                    TIMEBEFORE=$1;
+                    IsDateCorrect $TIMEBEFORE %Y-%m-%d
+                else 
+                    CallError "[TODO ERROR] Only one -b paraPAPAm";
+                fi
+                ;;
+            -s )
+                if [[ $s = false ]]
+                then
+                    if [[ $2 =~ ^[0-9]+$ ]]
+                    then
+                        s=true 
+                        WIDTH=$2
+                        shift;
+                        i=$((i + 1))
+                    else
+                        s=true
+                        WIDTH=-1
+                    fi
+                else
+                    CallError "[TODO ERROR] Only one -s";
+                fi
+                ;;
+            -g )
+                if [[ $g = false ]]
+                then
+                    if [[ $2 = "M" || $2 = "Z" ]]
+                    then
+                        GENDER=$2
+                        g=true
+                        shift;
+                        i=$((i + 1))
+                    else
+                        CallError "[TODO ERROR] -g needs gender (M|Z)";
+                    fi
+                else
+                    CallError "[TODO ERROR] Only one -g";
+                fi
+                ;;
+        esac
+        i=$((i + 1));
+        shift;
+    done    
+}
 
-    esac
-    i=$((i + 1));
-    shift 1;
-done
 
-echo "s:" $s ", width:" $WIDTH
-echo "a:" $a ", time after:" $TIMEAFTER
-echo "b:" $b ", time before:" $TIMEBEFORE   
-echo "c:" $c ", command:" $COMMAND
 
-echo "end"
+
+
+
+
+
+#===========================================================================
+#   Main
+#===========================================================================
+
+Main() {
+    c=false
+    a=false
+    b=false
+    s=false
+    g=false
+
+    FindParams $@
+
+
+    echo "s:" $s ", width:" $WIDTH
+    echo "a:" $a ", time after:" $TIMEAFTER
+    echo "b:" $b ", time before:" $TIMEBEFORE   
+    echo "c:" $c ", command:" $COMMAND
+    echo "g:" $g ", gender:" $GENDER
+
+    echo "end"
+}
+
+Main $@
